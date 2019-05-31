5ef12d3cd42862fc2513bd9e2388539d603b813c
brad-morrison <45197952+brad-morrison@users.noreply.github.com>
2019-05-31 20:55:22 +0100

    additions

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function clicked() {
+    console.log("clicked");
+    document.getElementById('over').style.visibility = "visible";
+}
+
+function closeWindow() {
+    console.log("close-clicked");
+    document.getElementById('over').style.visibility = "hidden";
+}

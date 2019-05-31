c84800e33255b2e316f036f399e8e705da6bd20d
brad-morrison <45197952+brad-morrison@users.noreply.github.com>
2019-05-31 21:39:56 +0100

    added window functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var A_Infantry = 0;
+
 function clicked() {
     console.log("clicked");
     document.getElementById('over').style.visibility = "visible";
@@ -6,4 +8,20 @@ function clicked() {
 function closeWindow() {
     console.log("close-clicked");
     document.getElementById('over').style.visibility = "hidden";
+}
+
+function addTo(type) {
+    
+    if (type == "infantry")
+    {
+        // create infantry row
+        var row = document.createElement("DIV");
+        row.innerHTML = "Infantry";
+        row.className = "row";
+        document.getElementById('scroll-main').appendChild(row);
+        
+        // set count label on home screen
+        A_Infantry++;
+        document.getElementById('A_Infantry_Count').innerHTML = A_Infantry;
+    }
 }

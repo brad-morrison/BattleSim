a8384c7a74a5f64ac88d11c3e6ebf636d4d96908
brad-morrison <45197952+brad-morrison@users.noreply.github.com>
2019-05-31 23:22:41 +0100

    d

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 var A_Infantry = 0;
-
+var rowObj =    '<p class="row-name"></p>' +
+                '<p class="row-rank"></p>' +
+                '<div class="row-buttons">' +
+                '<button class="row-button rankup-button">+</button>' +
+                '<button class="row-button rankdown-button">-</button>' +
+                '<button class="row-button delete-button">X</button>' +
+                '</div>';
+    
 function clicked() {
     console.log("clicked");
     document.getElementById('over').style.visibility = "visible";
@@ -15,13 +22,29 @@ function addTo(type) {
     if (type == "infantry")
     {
         // create infantry row
-        var row = document.createElement("DIV");
+        /*var row = document.createElement("DIV");
         row.innerHTML = "Infantry";
         row.className = "row";
-        document.getElementById('scroll-main').appendChild(row);
+        document.getElementById('scroll-main').appendChild(row);*/
+        createInfantry();
         
         // set count label on home screen
         A_Infantry++;
         document.getElementById('A_Infantry_Count').innerHTML = A_Infantry;
     }
+}
+
+function createInfantry() {
+    var row = {type: "Infantry", rank: "1"};
+    
+    // create row
+    var x = document.createElement("DIV");
+    x.innerHTML = rowObj;
+    x.id = "rowObjNew";
+    x.className = "row";
+    document.getElementById('scroll-main').appendChild(x);
+    
+    // add data to row
+    
+    
 }

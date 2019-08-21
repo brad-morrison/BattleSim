855e42310a4f8c46468e0ab7bba5272ae6e0acfd
brad-morrison <45197952+brad-morrison@users.noreply.github.com>
2019-08-21 19:48:54 +0100

    working with other player

--- js/main.js
+++ js/main.js
@@ -21,6 +21,10 @@ var a_infantry = [];
 var a_archers = [];
 var a_pikemen = [];
 var a_cavalry = [];
+var b_infantry = [];
+var b_archers = [];
+var b_pikemen = [];
+var b_cavalry = [];
 
 // UI variables
 var openWindow;
@@ -39,6 +43,10 @@ function closeWindow() {
     document.getElementById('A_Archers_Count').innerHTML = a_archers.length;
     document.getElementById('A_Pikemen_Count').innerHTML = a_pikemen.length;
     document.getElementById('A_Cavalry_Count').innerHTML = a_cavalry.length;
+    document.getElementById('B_Infantry_Count').innerHTML = b_infantry.length;
+    document.getElementById('B_Archers_Count').innerHTML = b_archers.length;
+    document.getElementById('B_Pikemen_Count').innerHTML = b_pikemen.length;
+    document.getElementById('B_Cavalry_Count').innerHTML = b_cavalry.length;
 }
 
 function loadWindowData(reg) {
@@ -50,30 +58,49 @@ function loadWindowData(reg) {
     document.getElementById('scroll-main').innerHTML = "";
     
     // set correct array
-    if (reg == "infantry")
+    if (reg == "a_infantry")
     {
         var unitSet = a_infantry;   
     }
     
-    if (reg == "archers")
+    if (reg == "a_archers")
     {
         var unitSet = a_archers;
     }
     
-    if (reg == "pikemen")
+    if (reg == "a_pikemen")
     {
         var unitSet = a_pikemen;
     }
     
-    if (reg == "cavalry")
+    if (reg == "a_cavalry")
     {
         var unitSet = a_cavalry;
+    }
+    
+    if (reg == "b_infantry")
+    {
+        var unitSet = b_infantry;   
+    }
+    
+    if (reg == "b_archers")
+    {
+        var unitSet = b_archers;
+    }
+    
+    if (reg == "b_pikemen")
+    {
+        var unitSet = b_pikemen;
+    }
+    
+    if (reg == "b_cavalry")
+    {
+        var unitSet = b_cavalry;
     }
     
     // loop to load data
     for (i=0; i<unitSet.length; i++)
     {
-        //createUnit(reg);
         fetchUnits(unitSet, i);
     }
     
@@ -101,7 +128,7 @@ function addTo() {
     
     var type = document.getElementById('over-title').innerHTML;
     
-    if (type == "infantry")
+    if (type == "a_infantry")
     {
         // create infantry row
         createUnit("infantry");
@@ -109,13 +136,25 @@ function addTo() {
         // add to array
         var freshUnit = new unit("infantry", 1);
         a_infantry.push(freshUnit);
-        console.log(a_infantry);
         
         // set count label on home screen
         document.getElementById('A_Infantry_Count').innerHTML = a_infantry.length;
     }
     
-    if (type == "archers")
+    if (type == "b_infantry")
+    {
+        // create infantry row
+        createUnit("infantry");
+        
+        // add to array
+        var freshUnit = new unit("infantry", 1);
+        b_infantry.push(freshUnit);
+        
+        // set count label on home screen
+        document.getElementById('B_Infantry_Count').innerHTML = b_infantry.length;
+    }
+    
+    if (type == "a_archers")
     {
         // create infantry row
         createUnit("archers");
@@ -123,13 +162,25 @@ function addTo() {
         // add to array
         var freshUnit = new unit("archers", 1);
         a_archers.push(freshUnit);
-        console.log(a_archers);
         
         // set count label on home screen
         document.getElementById('A_Archers_Count').innerHTML = a_archers.length;
     }
     
-    if (type == "pikemen")
+    if (type == "b_archers")
+    {
+        // create infantry row
+        createUnit("archers");
+        
+        // add to array
+        var freshUnit = new unit("archers", 1);
+        b_archers.push(freshUnit);
+        
+        // set count label on home screen
+        document.getElementById('B_Archers_Count').innerHTML = b_archers.length;
+    }
+    
+    if (type == "a_pikemen")
     {
         // create infantry row
         createUnit("pikemen");
@@ -137,13 +188,25 @@ function addTo() {
         // add to array
         var freshUnit = new unit("pikemen", 1);
         a_pikemen.push(freshUnit);
-        console.log(a_pikemen);
         
         // set count label on home screen
         document.getElementById('A_Pikemen_Count').innerHTML = a_pikemen.length;
     }
     
-    if (type == "cavalry")
+    if (type == "b_pikemen")
+    {
+        // create infantry row
+        createUnit("pikemen");
+        
+        // add to array
+        var freshUnit = new unit("pikemen", 1);
+        b_pikemen.push(freshUnit);
+        
+        // set count label on home screen
+        document.getElementById('B_Pikemen_Count').innerHTML = b_pikemen.length;
+    }
+    
+    if (type == "a_cavalry")
     {
         // create infantry row
         createUnit("cavalry");
@@ -151,10 +214,22 @@ function addTo() {
         // add to array
         var freshUnit = new unit("cavalry", 1);
         a_cavalry.push(freshUnit);
-        console.log(a_cavalry);
         
         // set count label on home screen
         document.getElementById('A_Cavalry_Count').innerHTML = a_cavalry.length;
+    }
+    
+    if (type == "b_cavalry")
+    {
+        // create infantry row
+        createUnit("cavalry");
+        
+        // add to array
+        var freshUnit = new unit("cavalry", 1);
+        b_cavalry.push(freshUnit);
+        
+        // set count label on home screen
+        document.getElementById('B_Cavalry_Count').innerHTML = b_cavalry.length;
     }
 }
 
@@ -186,24 +261,44 @@ function addRank(element) {
     var chosenIndex = rowName.slice(3);
     
     //if statements to change correct array item
-    if (openWindow == "infantry")
+    if (openWindow == "a_infantry")
     {
         if (a_infantry[chosenIndex-1].rank < 3) {a_infantry[chosenIndex-1].rank++;} // if rank is < 3 add to rank
     }
     
-    if (openWindow == "archers")
+    if (openWindow == "b_infantry")
+    {
+        if (b_infantry[chosenIndex-1].rank < 3) {b_infantry[chosenIndex-1].rank++;} // if rank is < 3 add to rank
+    }
+    
+    if (openWindow == "a_archers")
     {
         if (a_archers[chosenIndex-1].rank < 3) {a_archers[chosenIndex-1].rank++;}
     }
     
-    if (openWindow == "pikemen")
+    if (openWindow == "b_archers")
+    {
+        if (b_archers[chosenIndex-1].rank < 3) {b_archers[chosenIndex-1].rank++;}
+    }
+    
+    if (openWindow == "a_pikemen")
     {
         if (a_pikemen[chosenIndex-1].rank < 3) {a_pikemen[chosenIndex-1].rank++;}
     }
     
-    if (openWindow == "cavalry")
+    if (openWindow == "b_pikemen")
+    {
+        if (b_pikemen[chosenIndex-1].rank < 3) {b_pikemen[chosenIndex-1].rank++;}
+    }
+    
+    if (openWindow == "a_cavalry")
     {
         if (a_cavalry[chosenIndex-1].rank < 3) {a_cavalry[chosenIndex-1].rank++;}
+    }
+    
+    if (openWindow == "b_cavalry")
+    {
+        if (b_cavalry[chosenIndex-1].rank < 3) {b_cavalry[chosenIndex-1].rank++;}
     }
     
     
@@ -229,24 +324,44 @@ function lowerRank(element) {
     var chosenIndex = rowName.slice(3);
     
     //if statements to change correct array item
-    if (openWindow == "infantry")
+    if (openWindow == "a_infantry")
     {
         if (a_infantry[chosenIndex-1].rank >= 2) {a_infantry[chosenIndex-1].rank--;} // if rank is < 3 add to rank
     }
     
-    if (openWindow == "archers")
+    if (openWindow == "b_infantry")
+    {
+        if (b_infantry[chosenIndex-1].rank >= 2) {b_infantry[chosenIndex-1].rank--;} // if rank is < 3 add to rank
+    }
+    
+    if (openWindow == "a_archers")
     {
         if (a_archers[chosenIndex-1].rank >= 2) {a_archers[chosenIndex-1].rank--;}
     }
     
-    if (openWindow == "pikemen")
+    if (openWindow == "b_archers")
+    {
+        if (b_archers[chosenIndex-1].rank >= 2) {b_archers[chosenIndex-1].rank--;}
+    }
+    
+    if (openWindow == "a_pikemen")
     {
         if (a_pikemen[chosenIndex-1].rank >= 2) {a_pikemen[chosenIndex-1].rank--;}
     }
     
-    if (openWindow == "cavalry")
+    if (openWindow == "b_pikemen")
+    {
+        if (b_pikemen[chosenIndex-1].rank >= 2) {b_pikemen[chosenIndex-1].rank--;}
+    }
+    
+    if (openWindow == "a_cavalry")
     {
         if (a_cavalry[chosenIndex-1].rank >= 2) {a_cavalry[chosenIndex-1].rank--;}
+    }
+    
+    if (openWindow == "b_cavalry")
+    {
+        if (b_cavalry[chosenIndex-1].rank >= 2) {b_cavalry[chosenIndex-1].rank--;}
     }
     
     // HTML changes
@@ -270,24 +385,44 @@ function deleteRow(element) {
     var indexToRemove = rowName.slice(3);
     
     //if statements to remove from correct array
-    if (openWindow == "infantry")
+    if (openWindow == "a_infantry")
     {
         a_infantry.splice(indexToRemove-1, 1);
     }
     
-    if (openWindow == "archers")
+    if (openWindow == "b_infantry")
+    {
+        b_infantry.splice(indexToRemove-1, 1);
+    }
+    
+    if (openWindow == "a_archers")
     {
         a_archers.splice(indexToRemove-1, 1);
     }
     
-    if (openWindow == "pikemen")
+    if (openWindow == "b_archers")
+    {
+        b_archers.splice(indexToRemove-1, 1);
+    }
+    
+    if (openWindow == "a_pikemen")
     {
         a_pikemen.splice(indexToRemove-1, 1);
     }
     
-    if (openWindow == "cavalry")
+    if (openWindow == "b_pikemen")
+    {
+        b_pikemen.splice(indexToRemove-1, 1);
+    }
+    
+    if (openWindow == "a_cavalry")
     {
         a_cavalry.splice(indexToRemove-1, 1);
+    }
+    
+    if (openWindow == "b_cavalry")
+    {
+        b_cavalry.splice(indexToRemove-1, 1);
     }
     
     //get number of rows before deletion

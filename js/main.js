1504d2bade941c676f8f0f1e79c0ea2b80417e16
brad-morrison <45197952+brad-morrison@users.noreply.github.com>
2019-08-22 14:36:55 +0100

    d

--- js/main.js
+++ js/main.js
@@ -10,10 +10,14 @@ var rowObj =    '<p class="row-name"></p>' +
 /* JS Variables */
 
 // unit constructor
-function unit(type, rank)
+function unit(type, rank, soldierCount, attack, defense, speed)
 {
     this.type = type;
     this.rank = rank;
+    this.soldierCount = soldierCount;
+    this.attack = attack;
+    this.defense = defense;
+    this.speed = speed;
 }
 
 // arrays of units
@@ -28,6 +32,10 @@ var b_cavalry = [];
 
 // UI variables
 var openWindow;
+
+//debug
+var AS;
+var BS;
 
     
 function clicked(reg) {
@@ -134,7 +142,7 @@ function addTo() {
         createUnit("infantry");
         
         // add to array
-        var freshUnit = new unit("infantry", 1);
+        var freshUnit = new unit("infantry", 1, 20, 6, 8, 3);
         a_infantry.push(freshUnit);
         
         // set count label on home screen
@@ -147,7 +155,7 @@ function addTo() {
         createUnit("infantry");
         
         // add to array
-        var freshUnit = new unit("infantry", 1);
+        var freshUnit = new unit("infantry", 1, 20, 6, 8, 3);
         b_infantry.push(freshUnit);
         
         // set count label on home screen
@@ -160,7 +168,7 @@ function addTo() {
         createUnit("archers");
         
         // add to array
-        var freshUnit = new unit("archers", 1);
+        var freshUnit = new unit("archers", 1, 10, 8, 3, 6);
         a_archers.push(freshUnit);
         
         // set count label on home screen
@@ -173,7 +181,7 @@ function addTo() {
         createUnit("archers");
         
         // add to array
-        var freshUnit = new unit("archers", 1);
+        var freshUnit = new unit("archers", 1, 10, 8, 3, 6);
         b_archers.push(freshUnit);
         
         // set count label on home screen
@@ -186,7 +194,7 @@ function addTo() {
         createUnit("pikemen");
         
         // add to array
-        var freshUnit = new unit("pikemen", 1);
+        var freshUnit = new unit("pikemen", 1, 10, 6, 3, 10);
         a_pikemen.push(freshUnit);
         
         // set count label on home screen
@@ -199,7 +207,7 @@ function addTo() {
         createUnit("pikemen");
         
         // add to array
-        var freshUnit = new unit("pikemen", 1);
+        var freshUnit = new unit("pikemen", 1, 10, 6, 3, 10);
         b_pikemen.push(freshUnit);
         
         // set count label on home screen
@@ -212,7 +220,7 @@ function addTo() {
         createUnit("cavalry");
         
         // add to array
-        var freshUnit = new unit("cavalry", 1);
+        var freshUnit = new unit("cavalry", 1, 8, 6, 10, 3);
         a_cavalry.push(freshUnit);
         
         // set count label on home screen
@@ -225,13 +233,183 @@ function addTo() {
         createUnit("cavalry");
         
         // add to array
-        var freshUnit = new unit("cavalry", 1);
+        var freshUnit = new unit("cavalry", 1, 8, 6, 10, 3);
         b_cavalry.push(freshUnit);
         
         // set count label on home screen
         document.getElementById('B_Cavalry_Count').innerHTML = b_cavalry.length;
     }
+    
+    //DEBUG
+    createScore();
+    document.getElementById("debug-bonus-score-a").innerHTML = AS;
+    document.getElementById("debug-bonus-score-b").innerHTML = BS;
 }
+
+function plus(button)
+{
+    // A
+    if (button == "ai")
+    {
+        var freshUnit = new unit("infantry", 1, 20, 6, 8, 3);
+        a_infantry.push(freshUnit);
+        
+        // set count label on home screen
+        document.getElementById('A_Infantry_Count').innerHTML = a_infantry.length;
+    }
+    
+    if (button == "aa")
+    {
+        // add to array
+        var freshUnit = new unit("archers", 1, 10, 8, 3, 6);
+        a_archers.push(freshUnit);
+        
+        // set count label on home screen
+        document.getElementById('A_Archers_Count').innerHTML = a_archers.length;
+    }
+    
+    if (button == "ac")
+    {
+        // add to array
+        var freshUnit = new unit("cavalry", 1, 8, 6, 10, 3);
+        a_cavalry.push(freshUnit);
+        
+        // set count label on home screen
+        document.getElementById('A_Cavalry_Count').innerHTML = a_cavalry.length;
+    }
+    
+    if (button == "ap")
+    {
+        // add to array
+        var freshUnit = new unit("pikemen", 1, 10, 6, 3, 10);
+        a_pikemen.push(freshUnit);
+        
+        // set count label on home screen
+        document.getElementById('A_Pikemen_Count').innerHTML = a_pikemen.length;
+    }
+    
+    // B
+    if (button == "bi")
+    {
+        var freshUnit = new unit("infantry", 1, 20, 6, 8, 3);
+        b_infantry.push(freshUnit);
+        
+        // set count label on home screen
+        document.getElementById('B_Infantry_Count').innerHTML = b_infantry.length;
+    }
+    
+    if (button == "ba")
+    {
+        // add to array
+        var freshUnit = new unit("archers", 1, 10, 8, 3, 6);
+        b_archers.push(freshUnit);
+        
+        // set count label on home screen
+        document.getElementById('B_Archers_Count').innerHTML = b_archers.length;
+    }
+    
+    if (button == "bc")
+    {
+        // add to array
+        var freshUnit = new unit("cavalry", 1, 8, 6, 10, 3);
+        b_cavalry.push(freshUnit);
+        
+        // set count label on home screen
+        document.getElementById('B_Cavalry_Count').innerHTML = b_cavalry.length;
+    }
+    
+    if (button == "bp")
+    {
+        // add to array
+        var freshUnit = new unit("pikemen", 1, 10, 6, 3, 10);
+        b_pikemen.push(freshUnit);
+        
+        // set count label on home screen
+        document.getElementById('B_Pikemen_Count').innerHTML = b_pikemen.length;
+    }
+    
+    //DEBUG
+    createScore();
+    document.getElementById("debug-bonus-score-a").innerHTML = AS;
+    document.getElementById("debug-bonus-score-b").innerHTML = BS;
+    
+}
+
+function minus(button)
+{
+    // A
+    if (button == "ai")
+    {
+        a_infantry.pop();
+        
+        // set count label on home screen
+        document.getElementById('A_Infantry_Count').innerHTML = a_infantry.length;
+    }
+    
+    if (button == "aa")
+    {
+        a_archers.pop();
+        
+        // set count label on home screen
+        document.getElementById('A_Archers_Count').innerHTML = a_archers.length;
+    }
+    
+    if (button == "ac")
+    {
+        a_cavalry.pop();
+        
+        // set count label on home screen
+        document.getElementById('A_Cavalry_Count').innerHTML = a_cavalry.length;
+    }
+    
+    if (button == "ap")
+    {
+        a_pikemen.pop();
+        
+        // set count label on home screen
+        document.getElementById('A_Pikemen_Count').innerHTML = a_pikemen.length;
+    }
+    
+    // B
+    if (button == "bi")
+    {
+        b_infantry.pop();
+        
+        // set count label on home screen
+        document.getElementById('B_Infantry_Count').innerHTML = b_infantry.length;
+    }
+    
+    if (button == "ba")
+    {
+        b_archers.pop();
+        
+        // set count label on home screen
+        document.getElementById('B_Archers_Count').innerHTML = b_archers.length;
+    }
+    
+    if (button == "bc")
+    {
+        b_cavalry.pop();
+        
+        // set count label on home screen
+        document.getElementById('B_Cavalry_Count').innerHTML = b_cavalry.length;
+    }
+    
+    if (button == "bp")
+    {
+        b_pikemen.pop();
+        
+        // set count label on home screen
+        document.getElementById('B_Pikemen_Count').innerHTML = b_pikemen.length;
+    }
+    
+    //DEBUG
+    createScore();
+    document.getElementById("debug-bonus-score-a").innerHTML = AS;
+    document.getElementById("debug-bonus-score-b").innerHTML = BS;
+    
+}
+
 
 function createUnit(unitType) {
     var row0 = {type: unitType, rank: "Rank 1"};
@@ -314,6 +492,11 @@ function addRank(element) {
     {
         element.innerHTML = "Rank 3";
     }
+    
+    //DEBUG
+    createScore();
+    document.getElementById("debug-bonus-score-a").innerHTML = AS;
+    document.getElementById("debug-bonus-score-b").innerHTML = BS;
 }
 
 function lowerRank(element) {
@@ -376,6 +559,11 @@ function lowerRank(element) {
     {
         element.innerHTML = "Rank 1";
     }
+    
+    //DEBUG
+    createScore();
+    document.getElementById("debug-bonus-score-a").innerHTML = AS;
+    document.getElementById("debug-bonus-score-b").innerHTML = BS;
 }
 
 function deleteRow(element) {
@@ -438,5 +626,259 @@ function deleteRow(element) {
         console.log(rows[i]);
     }
     
+    //DEBUG
+    createScore();
+    document.getElementById("debug-bonus-score-a").innerHTML = AS;
+    document.getElementById("debug-bonus-score-b").innerHTML = BS;
     
+}
+
+// BATTLE LOGIC
+
+function battleLogic()
+{
+    
+    var playerScore = createScore();
+    
+    // check for win
+    var ACount = a_infantry.length + a_archers.length + a_pikemen.length + a_cavalry.length;
+    var BCount = b_infantry.length + b_archers.length + b_pikemen.length + b_cavalry.length;
+    
+    if (ACount == 0 || BCount == 0)
+    {
+        if (ACount != BCount)
+        {
+            if (ACount > 0)
+            {
+                console.log("A wins");
+            }
+            else
+            {
+                console.log("B wins");
+            }
+        }
+    }
+    
+    if (a_cavalry.length > 0)
+    {
+        if (b_pikemen.length > 0)
+        {
+            if (a_infantry.length > 0)
+            {
+                if (b_archers.length > 0)
+                {
+                    if (a_archers.length > 0)
+                    {
+                        //archers fire
+                        console.log("archers fire");
+                        return;
+                    }
+                    
+                    // charge infantry
+                    console.log("charge infantry");
+                    return;
+                }
+                
+                // charge infantry
+                console.log("charge infantry");
+                return;
+                
+            }
+            
+            if (a_archers.length > 0)
+            {
+                // archers fire
+                console.log("archers fire");
+                return;
+            }
+
+            if (a_pikemen.length > 0)
+            {
+                // charge pikes
+                console.log("charge pikes");
+                return;
+            }
+
+            // charge cavalry
+            console.log("charge cavalry");
+            return;
+        }
+        
+        // charge cavalry
+        console.log("charge cavalry");
+        return;
+    }
+    
+    if (a_infantry.length > 0)
+    {
+        if (b_archers.length > 0)
+        {
+            if (a_archers.length > 0)
+            {
+                //archers fire
+                console.log("archers fire");
+                return;
+            }
+
+            // charge infantry
+            console.log("charge infantry");
+            return;
+        }
+        
+        // charge infantry
+        console.log("charge infantry");
+        return;
+
+    }
+    
+    if (a_archers.length > 0)
+    {
+        // archers fire
+        console.log("archers fire");
+        return;
+    }
+
+    if (a_pikemen.length > 0)
+    {
+        // charge pikes
+        console.log("charge pikes");
+        return;
+    }
+    
+    // no units left
+    console.log("no units left");
+}
+
+function createScore()
+{
+    // calc base soldier score (number of soldiers * experience)
+    var aSoldierScore = calcSoldierScore(a_infantry) + calcSoldierScore(a_archers) + calcSoldierScore(a_cavalry) + calcSoldierScore(a_pikemen);
+    var bSoldierScore = calcSoldierScore(b_infantry) + calcSoldierScore(b_archers) + calcSoldierScore(b_cavalry) + calcSoldierScore(b_pikemen);
+    
+    // calc bonus score
+    var aBonusScore = calcBonus("A");
+    var bBonusScore = calcBonus("B");
+    
+    // calc final score
+    var aScore = aSoldierScore + aBonusScore;
+    var bScore = bSoldierScore + bBonusScore;
+    
+    //debug
+    AS = aScore;
+    BS = bScore;
+    
+    console.log("A score = " + aScore);
+    console.log("B score = " + bScore);
+}
+
+function calcSoldierScore(array)
+{
+    var soldierScore = 0;
+    
+    for (i=0; i < array.length; i++)
+    {
+        // soldier count * rank
+        soldierScore = soldierScore + (array[i].soldierCount * array[i].rank);
+        
+        // add attack, defense and speed scores
+        soldierScore = soldierScore + (array[i].attack + array[i].defense + array[i].speed);
+    }
+    
+    return soldierScore;
+}
+
+function calcBonus(checking)
+{
+    var bonus = 0;
+    
+    // check correct arrays
+    if (checking == "A")
+    {
+        var aI = a_infantry;
+        var aA = a_archers;
+        var aC = a_cavalry;
+        var aP = a_pikemen;
+        var bI = b_infantry;
+        var bA = b_archers;
+        var bC = b_cavalry;
+        var bP = b_pikemen;
+    }
+    else
+    {
+        var aI = b_infantry;
+        var aA = b_archers;
+        var aC = b_cavalry;
+        var aP = b_pikemen;
+        var bI = a_infantry;
+        var bA = a_archers;
+        var bC = a_cavalry;
+        var bP = a_pikemen;
+    }
+    
+    // if A has cavalry and B has no pikes
+    if (aC.length > 0 && bP.length == 0)
+    {
+        if (aC.length >= bI.length)
+        {
+            bonus = bonus + 200;
+        }
+        else
+        {
+            bonus = bonus + 80;
+        }
+    }
+    
+    // if A has pikes and B has cavalry
+    if (aP.length > 0 && bC.length > 0)
+    {
+        if (aP.length >= bC.length)
+        {
+            bonus = bonus + 200;
+        }
+        else
+        {
+            bonus = bonus + 80;
+        }
+    }
+    
+    // if A has archers and b has no archers or cavalry
+    if (aA.length > 0 && (bA.length == 0 && bC.length == 0))
+    {
+        if (aA.length >= bI.length)
+        {
+            bonus = bonus + 100;
+        }
+        else
+        {
+            bonus = bonus + 60;
+        }
+    }
+    
+    // if A has infantry and B has archers but no cavalry
+    if (aI.length > 0 && (bA.length > 0 && bC.length == 0))
+    {
+        if (aI.length >= bA.length)
+        {
+            bonus = bonus + 100;
+        }
+        else
+        {
+            bonus = bonus + 40;
+        }
+    }
+    
+    // if A has infantry and B has pikes
+    if (aI.length > 0 && bP.length > 0)
+    {
+        if (aI.length >= bP.length)
+        {
+            bonus = bonus + 100;
+        }
+        else 
+        {
+            bonus = bonus + 40;
+        }
+    }
+    
+    return bonus;
 }
